e884db35528f73b89d44c55857da2d40e48cfb4a
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2019-09-20 16:41:26 +0700

    v0.3

--- cardvalidate/README.md
+++
@@ -1,3 +0,0 @@
-Credit Card Validator
-
-A Python program to identify credit card issuer and validate card number using Luhn's algorithm
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-    print(long_description)
+long_description = """Credit Card Validator
+
+A Python program to identify credit card issuer and validate card number using Luhn's algorithm"""
 
 setup(
   name = 'cardvalidate',         # How you named your package folder (MyLib)
   packages = ['cardvalidate'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Credit card number and CVV validator and checker',   # Give a short description about your library
   long_description=long_description,
@@ -15,7 +15,7 @@ setup(
   author = 'sawit.tr@gmail.com',                   # Type in your name
   author_email = 'sawit.tr@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/tansawit/cardvalidate',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/tansawit/cardvalidate/archive/0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/tansawit/cardvalidate/archive/0.1.1.tar.gz',    # I explain this later on
   keywords = ['credit', 'card', 'validator','checker','issuer'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           're',
