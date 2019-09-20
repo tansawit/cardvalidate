6e250ac2a0efcdb3232be24b74926baa7c665f4a
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2019-09-20 16:43:38 +0700

    removed re from install_requires

--- setup.py
+++ setup.py
@@ -15,11 +15,9 @@ setup(
   author = 'sawit.tr@gmail.com',                   # Type in your name
   author_email = 'sawit.tr@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/tansawit/cardvalidate',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/tansawit/cardvalidate/archive/0.1.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/tansawit/cardvalidate/archive/0.3.tar.gz',    # I explain this later on
   keywords = ['credit', 'card', 'validator','checker','issuer'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          're',
-      ],
+  install_requires=[]            # I get to this in a second,
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
