3c5cc36e9d817473fd2b95d8991ff40ee636c174
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2019-09-21 19:16:26 +0700

    updated setup to latest release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ A Python program to identify credit card issuer and validate card number using L
 setup(
   name = 'cardvalidate',         # How you named your package folder (MyLib)
   packages = ['cardvalidate'],   # Chose the same as "name"
-  version = '0.3',      # Start with a small number and increase it with every change you make
+  version = '0.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Credit card number and CVV validator and checker',   # Give a short description about your library
   long_description=long_description,
